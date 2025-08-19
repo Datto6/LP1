c243ca96f583f7829e403a1e0e32a386d7136e8b
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-08-19 14:37:34 -0300

    Create times2.c

---
+++ times2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+struct Time{   //objeto para comparar times, depois poss
+        char nome[3];
+        int vitorias;
+        int derrotas;
+        int empates;
+        int pontos;
+    };
+
+int main(void){
+    struct Time lista_times[20];
+    for (int i=1;i<=3;i++){
+        struct Time time_novo;
+        scanf("%s",time_novo.nome);
+        scanf("%d",&time_novo.vitorias);
+        scanf("%d",&time_novo.derrotas);
+        scanf("%d",&time_novo.empates);
+        time_novo.pontos=time_novo.vitorias*3+time_novo.empates;
+        lista_times[i]=time_novo;
+    }
+    int min=50000; //valores arbitrários razoáveis para mínimo e máximo
+    int max=-1;
+    char nome_max[3];
+    char nome_min[3];
+    for (int i =1;i<=3;i++){
+
+        if (lista_times[i].pontos>max){
+            max=lista_times[i].pontos;
+            for (int j=0;j<3;j++){ //e executado toda vez?
+                nome_max[j]=lista_times[i].nome[j];
+            }
+        }
+        else if (lista_times[i].pontos<min){
+            min=lista_times[i].pontos;
+            for (int j=0;j<3;j++){
+                nome_min[j]=lista_times[i].nome[j];
+            }
+        }
+        printf("%s\n",nome_max);
+    }
+    printf("%d e o maximo de pontos, atingido pelo time %s\n",max,nome_max); //da erro no nome do max
+    printf("%d e o minimo de pontos, atingido pelo time %s\n",min, nome_min);
+    return 0;
+}
