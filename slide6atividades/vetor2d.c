944f6a8e94cf5b8c0eacb80a8b3d95234c41723a
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-09-09 12:19:28 -0500

    nao terminei ainda, mas fazendo funcoes de preencher e media de vetores bidimensionais

---
+++ slide6atividades/vetor2d.c
@@ -0,0 +1,16 @@
+void preenche (int* arr, int l, int c){
+    for (int i=0;i<l;i++){
+        for (int j=0;j<c;j++){
+            scanf("%d",(arr+j*l+j));
+        }
+    }
+}
+
+float media (int* arr, int l, int c){
+    long int soma=0;
+    for (int i=0;i<l;i++){
+        for (int j=0;j<c;j++){
+            scanf("%d",(arr+j*l+j));
+        }
+    }
+}
