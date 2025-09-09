8177c96cefe3863cdf261709127b449a13829b60
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-09-09 12:04:29 -0500

    exercicio 2 tirar 1 elemento no indice I sem buracos

---
+++ slide6atividades/tiravetor.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+void printar(int* vet, int numero){
+    for (int i=0;i<numero;i++){
+        printf("%d ",vet[i]);
+    }
+    printf("\n");
+}
+int main(void){
+    int vet[5];
+    for (int i=0;i<5;i++){
+        scanf("%d",vet+i);
+    }
+    printar(vet,5);
+    int numero;
+    scanf("%d",&numero);
+    for (int i=numero+1;i<4;i++){ //comecar uma unidade dps do alvo
+        vet[i-1]=vet[i]; //troca o anterior pelo atual
+        vet[i]=vet[i+1]; //atual pelo proximo
+    }
+    vet[4]=0; //colocar zero no final. Com N elementos removidos, logica fica mais dificil
+    printar(vet,5);
+    return 0;
+}
