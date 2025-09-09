9a5f2fda47cff70466d8dfaccf2f2a6ce8c74e08
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-09-09 12:34:11 -0500

    finalizacao das 2 funcoes, sem ter testado antes

--- slide6atividades/vetor2d.c
+++ slide6atividades/vetor2d.c
@@ -10,7 +10,10 @@ float media (int* arr, int l, int c){
     long int soma=0;
     for (int i=0;i<l;i++){
         for (int j=0;j<c;j++){
-            scanf("%d",(arr+j*l+j));
+            soma+= *(arr+j*l+j);
         }
     }
+    int divisor=l*c;
+    float retorno=(float) soma/divisor;
+    return retorno;
 }
