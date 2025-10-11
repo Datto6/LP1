209800e3cd663a0766c974dcd061ffaa53b2881b
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-10-10 21:14:11 -0300

    exercicio7 pessoas escrito e lido em "binario"

--- slide8atividades/pessoasbin.c
+++ slide8atividades/pessoasbin.c
@@ -12,17 +12,51 @@ void preenchePessoa(int idade, char* nome, int peso, Pessoa* ptr){
     strncpy(ptr->nome,nome,64);
     ptr->peso=peso;
 }
-
+void exibePessoa(Pessoa coisa){
+    printf("%s",coisa.nome);
+    printf(" Sua idade: %d",coisa.idade);
+    printf(" Seu peso: %d \n",coisa.peso);
+}
+void escrevePessoa(Pessoa coisa,FILE* f){
+    fwrite(&coisa.idade,sizeof(int),1,f);
+    fwrite(coisa.nome,sizeof(char),64,f);
+    fwrite(&coisa.peso,sizeof(int),1,f);
+}
+Pessoa lePessoa(FILE* f){
+    Pessoa coisa;
+    fread(&coisa.idade,sizeof(int),1,f);
+    fread(coisa.nome,sizeof(char),64,f);
+    fread(&coisa.peso,sizeof(int),1,f);
+    return coisa;
+}
 int main(void){
     Pessoa vec[10];
-    for (int i=0;i<10;i++){
-        int idade;
-        int peso;
-        char nome[64];
-        scanf("%d",&idade);
-        scanf("%d",&peso);
-        scanf("%s",nome);
-        preenchePessoa(idade,nome,peso,vec+i);
+    FILE* arquivo=fopen("C:\\LP1\\pessoas.bin","rb+");
+    int ler;
+    printf("Voce quer escrever ou ler conteudo? 1 para escrever e 0 p ler");
+    scanf("%d",&ler);
+    if (ler){
+        for (int i=0;i<3;i++){
+            Pessoa coisa;
+            vec[i]=coisa;
+            int idade;
+            int peso;
+            char nome[64];
+            scanf("%d",&idade);
+            scanf("%d",&peso);
+            scanf("%s",nome);
+            preenchePessoa(idade,nome,peso,vec+i);
+            escrevePessoa(vec[i],arquivo);
+        }
     }
+    else{
+        for (int i=0;i<3;i++){
+            Pessoa coisa=lePessoa(arquivo);
+            vec[i]=coisa;
+            exibePessoa(vec[i]);
+        }
+    }
+
+    fclose(arquivo);
     return 0;
 }
