03ddb28fbf451e2cc4678d05c024d5175d10cb63
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-11-02 13:00:10 -0300

    ler a linha x do arquivo pedido

---
+++ slide8navegacao/exercicio1linhas.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <assert.h>
+int main(void){
+    int linha;
+    scanf("%d",&linha);
+    FILE* arq=fopen("ex1.txt","r");
+    fseek(arq,23*(linha-1)+9,SEEK_SET); //pegar idade
+    int estado[2]; char estadovisivel[3];
+    int idade[3]; char idadevisivel[4];
+
+    estadovisivel[2]='\0';
+    for(int i=0;i<3;i++){
+        idade[i]=fgetc(arq);
+        if (idade[i]==EOF){
+            printf("Registro nao encontrado\n");
+            assert(0);
+        }
+        
+        idadevisivel[i]=(char) idade[i];
+    }
+    idadevisivel[3]='\0';
+
+    fseek(arq,23*(linha-1)+19,SEEK_SET);
+    for(int i=0;i<2;i++){
+        estado[i]=fgetc(arq);
+        if (estado[i]==EOF){
+            printf("Registro nao encontrado\n");
+            assert(0);
+        }
+        estadovisivel[i]=(char) estado[i];
+    }
+    printf("%s\n",idadevisivel);
+    printf("%s\n",estadovisivel);
+    fclose(arq);
+    return 0;
+}
