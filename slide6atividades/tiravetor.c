41b389d5f455bb16aba9d6f4a69f86e63cc60f09
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-11-06 11:56:39 -0500

    nova variação, o pedido foi refazer então dando uma retocada

--- slide6atividades/tiravetor.c
+++ slide6atividades/tiravetor.c
@@ -8,14 +8,13 @@ void printar(int* vet, int numero){
 int main(void){
     int vet[5];
     for (int i=0;i<5;i++){
-        scanf("%d",vet+i);
+        scanf("%d",&vet[i]);
     }
     printar(vet,5);
     int numero;
     scanf("%d",&numero);
-    for (int i=numero+1;i<4;i++){ //comecar uma unidade dps do alvo
-        vet[i-1]=vet[i]; //troca o anterior pelo atual
-        vet[i]=vet[i+1]; //atual pelo proximo
+    for (int i=numero+1;i<5;i++){ //comecar uma unidade dps do alvo
+        vet[i-1]=vet[i]; //troca o anterior pelo atual, deslocando todos 1 p esquerda
     }
     vet[4]=0; //colocar zero no final. Com N elementos removidos, logica fica mais dificil
     printar(vet,5);
