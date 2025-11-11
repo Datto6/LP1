5f864fa39c4263cf3ab7bf1a10ba8d2f852a17a9
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-11-11 11:58:57 -0500

    fazendo apenas o pedido no enunciado da número 1

--- slide9listas/listaenc1.c
+++ slide9listas/listaenc1.c
@@ -4,13 +4,14 @@ typedef struct Caixa{
     struct Caixa* prox;
 }Caixa;
 
-Caixa constroi(Caixa* prox){ 
+Caixa constroi(Caixa* prox,int x){ 
     Caixa saida;
     saida.prox=prox; //nao "aloca" proximo sozinho, precisa que o programador ja informe
-    scanf("%d",&saida.valor); //pega valor com usuario
+    //scanf("%d",&saida.valor); pega valor com usuario
+    saida.valor=x;
     return saida; //retorna variavel local, precisa ser atribuido a um externo fora da função
 }
-void printar(Caixa* coisa){
+void exibe(Caixa* coisa){
     Caixa* p=coisa;
     while(p!=NULL){ //enquanto nao chegou ao final
         printf("%d",p->valor);
@@ -22,11 +23,11 @@ void printar(Caixa* coisa){
 int main(void){
     Caixa coisa1;  Caixa coisa2;
     Caixa coisa3; Caixa coisa4; Caixa coisa5; //declaracoes de caixas
-    coisa1=constroi(&coisa2);
-    coisa2=constroi(&coisa3);
-    coisa3=constroi(&coisa4);
-    coisa4=constroi(&coisa5);
-    coisa5=constroi(NULL); //NULL porque e o ultimo elemento da lista
-    printar(&coisa1);
+    coisa1=constroi(&coisa2,1);
+    coisa2=constroi(&coisa3,3);
+    coisa3=constroi(&coisa4,9);
+    coisa4=constroi(&coisa5,7);
+    coisa5=constroi(NULL,5); //NULL porque e o ultimo elemento da lista
+    //exibe(&coisa1);
     return 0;
 }
