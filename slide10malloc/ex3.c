9c1bdb13409a4e02aaa485e24f4ae7e769c3d476
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-11-18 12:52:24 -0500

    remocao do elemento no indice 2

---
+++ slide10malloc/ex3.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Caixa {
+    int valor;
+    struct Caixa* prox;
+} Caixa ;
+
+void InsereFinal(Caixa ** L, int e, Caixa ** u) {  
+	Caixa * novo = (Caixa *) malloc(sizeof(Caixa)); //aloca as caixas aqui
+	novo->valor = e; novo->prox = NULL; 
+	if (*u == NULL) {
+		*L = novo; //se o ultimo nao existir, comeca lista ali
+	} else {
+		(*u)->prox = novo; //ultimo agora e o novo 
+	}
+	*u = novo;  //atualiza o novo ultimo da lista 
+}
+void exibe(Caixa* coisa){
+    Caixa* p=coisa;
+    while(p!=NULL){ //enquanto nao chegou ao final
+        printf("%d",p->valor);
+        p=p->prox;
+        if (p!=NULL) printf(" ->"); 
+    }
+    printf("\n");
+}
+int main(void){
+    Caixa* cabeca=NULL;
+    Caixa* ultimo=NULL;
+    InsereFinal(&cabeca,1,&ultimo);
+    InsereFinal(&cabeca,3,&ultimo);
+    InsereFinal(&cabeca,9,&ultimo);
+    InsereFinal(&cabeca,7,&ultimo);
+    InsereFinal(&cabeca,5,&ultimo);
+    exibe(cabeca);
+    //inicio de remocao
+    Caixa* p=cabeca;
+    Caixa* pAnt=NULL;
+    int indice=0;
+    while(p!=NULL){ //percorre a lista 
+        if (indice==2){
+            pAnt->prox=p->prox;
+            free(p);
+            break;
+        }
+        pAnt=p;
+        p=p->prox;
+        indice++;
+    }
+    exibe(cabeca);
+    return 0;
+}
