6546b6aaf4fd389000ebf52de1827c317b8c8fa3
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-11-18 13:00:41 -0500

    mesma coisa de contem de slide 9 sem o malloc, essa nem precisava mudar

---
+++ slide10malloc/ex4contem.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Caixa {
+    int valor;
+    struct Caixa* prox;
+} Caixa ;
+
+void InsereFinal(Caixa ** L, int e, Caixa ** u) {  
+	Caixa * novo = (Caixa *) malloc(sizeof(Caixa)); //aloca as caixas aqui
+	novo->valor = e; novo->prox = NULL; 
+	if (*u == NULL) {
+		*L = novo; //se o ultimo nao existir, comeca lista ali
+	} else {
+		(*u)->prox = novo; //ultimo agora e o novo 
+	}
+	*u = novo;  //atualiza o novo ultimo da lista 
+}
+void InsereInicioEnc(Caixa ** L, int e) { //funcoes auxiliares 
+	Caixa * novo = (Caixa *) malloc(sizeof(Caixa));
+	novo->valor = e; novo->prox = *L; *L = novo; 
+}
+void exibe(Caixa* coisa){
+    Caixa* p=coisa;
+    while(p!=NULL){ //enquanto nao chegou ao final
+        printf("%d",p->valor);
+        p=p->prox;
+        if (p!=NULL) printf(" ->"); 
+    }
+    printf("\n");
+}
+int contem(Caixa* coisa,int valor){
+    Caixa* p=coisa;
+    while(p!=NULL){
+        if(p->valor==valor){
+            return 1; //achou, logo contem e true
+        }
+        p=p->prox;
+    }
+    return 0; //nao achou, logo nao contem
+}
+
+int main(void){
+    Caixa* cabeca=NULL;
+    Caixa* ultimo=NULL;
+    InsereFinal(&cabeca,1,&ultimo);
+    InsereFinal(&cabeca,3,&ultimo);
+    InsereFinal(&cabeca,9,&ultimo);
+    InsereFinal(&cabeca,7,&ultimo);
+    InsereFinal(&cabeca,5,&ultimo);
+    exibe(cabeca);
+    printf("%d\n",contem(cabeca,9)); //deve retornar 1, pois 9 ainda ta la dentro
+    Caixa* p=cabeca;
+    Caixa* pAnt=NULL;
+    int indice=0;
+    while(p!=NULL){ //percorre a lista 
+        if (indice==2){
+            pAnt->prox=p->prox;
+            free(p);
+            break;
+        }
+        pAnt=p;
+        p=p->prox;
+        indice++;
+    }
+    exibe(cabeca);
+    printf("%d\n",contem(cabeca,9)); //deve retornar 0, pois acabei de tirar o 9 de la
+    InsereInicioEnc(&cabeca,9);
+    exibe(cabeca);
+    printf("%d\n",contem(cabeca,9)); //retornar 1 de novo, coloquei de volta
+    return 0;
+}
