6d2d0b1d55e9ff8e9b60c1a9bea1f00f48bf30b0
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-12-04 11:56:33 -0500

    atualizando meio, agora nao perde o character antigo do meio. Ainda sem usar loops

--- slide8navegacao/ex3meio.c
+++ slide8navegacao/ex3meio.c
@@ -3,8 +3,12 @@
 void meio(FILE* arq,char inserido){
     fseek(arq, 0, SEEK_END);           
     long int fim = ftell(arq);          
-    fseek(arq, fim / 2, SEEK_SET);      
-    fputc(inserido, arq); 
+    fseek(arq, fim / 2, SEEK_SET);
+    char* depois_metade[fim/2]; //alocacao dinamica, posso usar malloc aqui? nao encontrei muito outro jeito
+    fread(depois_metade,sizeof(char),fim/2,arq); //ler todos os chars depois da metade
+    fseek(arq, (fim / 2 ), SEEK_SET);     //voltar p metade
+    fputc(inserido, arq);  //colocar o char desejado no meio
+    fwrite(depois_metade,sizeof(char),fim/2,arq);  //colocar os outros da metade um para frente
 }
 
 long int tamanho(FILE* arq){
