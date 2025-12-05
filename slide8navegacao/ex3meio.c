2fce3905b3a05f0d8fb99a6c21b72931ad1ee319
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-12-05 11:14:50 -0300

    achava que tinha resolvido, agora que foi de fato, tava calculando metade e quantidade errado

--- slide8navegacao/ex3meio.c
+++ slide8navegacao/ex3meio.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void meio(FILE* arq,char inserido){
     fseek(arq, 0, SEEK_END);           
-    long int fim = ftell(arq);          
-    fseek(arq, fim / 2, SEEK_SET);
-    char* depois_metade[fim/2]; //alocacao dinamica, posso usar malloc aqui? nao encontrei muito outro jeito
-    fread(depois_metade,sizeof(char),fim/2,arq); //ler todos os chars depois da metade
-    fseek(arq, (fim / 2 ), SEEK_SET);     //voltar p metade
-    fputc(inserido, arq);  //colocar o char desejado no meio
-    fwrite(depois_metade,sizeof(char),fim/2,arq);  //colocar os outros da metade um para frente
+    long int fim = ftell(arq); 
+    long int metade=fim/2;
+    long int qntd=fim- metade;
+
+    fseek(arq, metade, SEEK_SET);
+    char* depois_metade=malloc(qntd); //alocacao dinamica, posso usar malloc aqui? nao encontrei muito outro jeito
+    
+    fread(depois_metade,sizeof(char),qntd,arq); //ler todos os chars depois da metade
+    fseek(arq, metade, SEEK_SET);     //voltar p metade
+    fwrite(&inserido,sizeof(char),1, arq);  //colocar o char desejado no meio
+
+
+    fwrite(depois_metade,sizeof(char),qntd,arq);  //colocar os outros da metade um para frente
 }
 
 long int tamanho(FILE* arq){
@@ -21,7 +28,7 @@ long int tamanho(FILE* arq){
     return tamanho;
 }
 int main(void){
-    FILE* arq=fopen("ex2.txt","r+");
+    FILE* arq=fopen("ex2.txt","rb+");
     long int teste=tamanho(arq);
     printf("%ld\n",teste);
     meio(arq,'A');
