6f751c39cf184aada52106413302a1d050b5ef15
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-12-08 17:33:59 -0300

    adcionando documentacao

--- slide8navegacao/ex2tamanho.c
+++ slide8navegacao/ex2tamanho.c
@@ -4,8 +4,8 @@
 long int tamanho(FILE* arq){
     long int tamanho=0;
     int desnecessario;
-    fseek(arq,0,SEEK_SET);
-    while(fgetc(arq)!=EOF){
+    fseek(arq,0,SEEK_SET); //vai ate o inicio do arquivo
+    while(fgetc(arq)!=EOF){ //enquanto o retorno nao eh EOF, aumenta o tamanho
         tamanho++;
     }
     return tamanho;
