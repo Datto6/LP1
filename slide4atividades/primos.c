1561594aa26d8bd4af365e820064bce9f657d595
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-12-09 09:59:52 -0300

    documentacao de primos

--- slide4atividades/primos.c
+++ slide4atividades/primos.c
@@ -2,11 +2,11 @@
 #include <math.h>
 int eh_primo(int n){
     for (int i=2;i<=sqrt(n);i++){
-        if (n%i==0){
+        if (n%i==0){ //se algum desses numeros dividir o n, n nao eh primo
             return 0;
         }
     }
-    return 1;
+    return 1; //se sobreviveu ao loop, eh primo
 }
 
 void todos_os_primos(int max){
