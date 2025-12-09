7df6533ed3d77c24a5dbfaa2e8e12341d3caf5a6
Datto6 <61701645+Datto6@users.noreply.github.com>
2025-12-09 10:55:24 -0300

    documentacao

--- slide5atividades/troca.c
+++ slide5atividades/troca.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 void troca(int* x, int* y){
     int temp=*x;
-    *x=*y;
-    *y=temp;
+    *x=*y;  //dereferencia o valor do ponteiro x, mexe no valor a qual o endereco se refere
+    *y=temp; //bota o antigo valor de *x em y 
 }
 int main(void){
     int x=10, y=20;
